cad783f3eb3ba934f0f62444d83b76065d91578f
Rayan Raihani <rayanraihani9@gmail.com>
2022-11-03 11:46:58 +0100

    Exo 6

--- exercice-6.php
+++ exercice-6.php
@@ -1,9 +1,18 @@
 <?php
 $prixuniter = 9.99;
+echo "Prix unitaire de l’article: $prixuniter"."<br>";
 $nombre = 5;
+echo "Quantité: $nombre"."<br>";
 $tva = 0.2;
+echo "Taux de TVA: $tva"."<br>";
 $total = $nombre*$prixuniter ;
-$montant = $total*$tva;
-$facture = $total+ $montant;
-echo "Le montant de la facture à régler est de : $facture";
+
+$montantTVA = $total*$tva;
+
+$facture = $total + $montantTVA;
+
+number_format($facture,2);
+
+echo "Le montant de la facture à régler est de : ".$facture;
+
 ?>
