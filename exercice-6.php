bb41d42ad8d1cfb96c185f71237fca4731d3689b
Rayan Raihani <rayanraihani9@gmail.com>
2022-11-03 09:27:44 +0100

    exo 6

--- exercice-6.php
+++ exercice-6.php
@@ -1,3 +1,9 @@
 <?php
-echo "My first PHP script!";
+$prixuniter = 9.99;
+$nombre = 5;
+$tva = 0.2;
+$total = $nombre*$prixuniter ;
+$montant = $total*$tva;
+$facture = $total+ $montant;
+echo "Le montant de la facture à régler est de : $facture";
 ?>
