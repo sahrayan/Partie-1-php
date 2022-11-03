e60ba53c373f5e5b7cd580d5bc01a8459ce90601
Rayan Raihani <rayanraihani9@gmail.com>
2022-11-03 11:23:58 +0100

    Exo4

--- exercice-4.php
+++ exercice-4.php
@@ -3,8 +3,9 @@
 
 $mot = 'Engage le jeu que je le gagne';
 
-$inverse = strtolower(strrev($mot));
-if (strtolower($mot) === $inverse) {
+$inverse = strtolower(str_replace(" ","",$mot));
+$inverse= strrev($inverse);
+if (strtolower(str_replace(" ","",$mot)) === $inverse) {
     echo $mot . ' est palindrome';
 } else {
     echo $mot . ' n\'est pas palindrome ';
