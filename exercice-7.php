beee4ce9a243cc7488b841ad1e869d8d37a708df
Rayan RAIHANI <rayanraihani9@gmail.com>
2022-11-08 16:51:09 +0100

    exercice-7.php

--- exercice-7.php
+++ exercice-7.php
@@ -1,9 +1,19 @@
 <?php
-$age = 8
-if ($age == 6 || $age==7){ //"||" signifie "ou"
-    echo "Catégorie Poussin"
-    }elseif ($age == 8 || $age==9)
-    echo "Catégorie Pupille"
+$age = 15;
 
+if ($age == 6 || $age==7){
 
+    echo "Catégorie Poussin";
+
+}elseif ($age == 8 || $age==9){
+
+    echo "Catégorie Pupille";
+
+}elseif ($age == 10 || $age==11){
+
+    echo "Catégorie Minime";
+
+}elseif ($age >= 12)
+
+echo "Catégorie Cadet";
 ?>
