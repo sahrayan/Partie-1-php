e16555af1e992c7e3349a04cf426b85ee5cdf255
Rayan R <rayanraihani9@gmail.com>
2022-11-09 13:58:51 +0100

    exercice-10.php

---
+++ exercice-10.php
@@ -0,0 +1,10 @@
+<?php
+$montantapayer = 152;
+ echo "Montant à payer : ".$montantapayer. "€". "<br>";
+$montantverser = 200;
+echo "Montant versé : ".$montantverser. "€". "<br>";
+$rendu = $montantverser-$montantapayer;
+echo  "Reste a payer : ".$rendu. "€". "<br>";
+echo "**************";
+
+?>
