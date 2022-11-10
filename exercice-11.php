823b72879f28a2a31c0b9a06349b8c50f7f2ed84
Rayan R <rayanraihani9@gmail.com>
2022-11-10 09:37:22 +0100

    exo11

---
+++ exercice-11.php
@@ -0,0 +1,8 @@
+<?php
+echo "Il y a 4 marques de voitures dans le tableau : <br>";
+$cars = array('Peugeot','Renault' ,'BMW' ,'Mercedes');
+echo $cars[0] ."<br>";
+echo $cars[1] ."<br>";
+echo $cars[2] ."<br>";
+echo $cars[3] ."<br>";
+?>
