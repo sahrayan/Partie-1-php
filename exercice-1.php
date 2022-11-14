1448fa03f48282e95028c8953f90c74561125208
Rayan <rayanraihani9@gmail.com>
2022-11-14 09:00:32 +0100

    test commit

--- exercice-1.php
+++ exercice-1.php
@@ -11,3 +11,5 @@ $phrase = "Notre formation commence aujourd'huit";
 $longueur = strlen($phrase); //strlen = permets de compter le nombre de mots.
 echo "la phrase « $phrase » contient $longueur caractères<br>";
 echo "la phrase « $phrase » contien ". strlen($phrase). " caractères<br>";
+
+echo "test";
