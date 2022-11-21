7955ac07be35e4d4df625d2090a237d6441c03fe
Rayan R <rayanraihani9@gmail.com>
2022-11-21 14:20:09 +0100

    exo11

--- exercice-11.php
+++ exercice-11.php
@@ -1,8 +1,6 @@
 <?php
 echo "Il y a 4 marques de voitures dans le tableau : <br>";
 $cars = array('Peugeot','Renault' ,'BMW' ,'Mercedes');
-echo $cars[0] ."<br>";
-echo $cars[1] ."<br>";
-echo $cars[2] ."<br>";
-echo $cars[3] ."<br>";
+$nombre =count($cars);
+echo $cars[0] ."<br>".$cars[1] ."<br>".$cars[2] ."<br>".$cars[3] ."<br>";
 ?>
